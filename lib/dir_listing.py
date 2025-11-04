5dc79deb4c51442ad83a07d369a9a25d68dbb627
Mark Nottingham <mnot@mnot.net>
2025-11-04 17:00:53 -0500

    not with this stylesheet

--- lib/dir_listing.py
+++ lib/dir_listing.py
@@ -13,7 +13,8 @@ wgname = "aipref"
 
 summary_header = f"""
 
-    **NOTE**: _This is a non-normative, AI-generated summary supplied only for convenience; it does not necessarily represent an accurate record of the meeting. See the minutes for the authoriative record. See [the source](https://ietfminutes.org/) for more information._
+**NOTE**: _This is a non-normative, AI-generated summary supplied only for convenience; it does not necessarily represent an accurate record of the meeting. See the minutes for the authoriative record. See [the source](https://ietfminutes.org/) for more information._
+
 """
 
 def spider(directory, reverse=False):
